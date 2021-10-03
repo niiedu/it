322f8160ed5a8da8eaf4639ed554be39ded0cd62
Maeda Akira <maeda.akir.72@gmail.com>
2021-10-03 09:02:21 +0900

    updte javscript

--- sample-javascript/movie/d-biblio/d-biblio.js
+++ sample-javascript/movie/d-biblio/d-biblio.js
@@ -10,12 +10,12 @@ window.onload = function() {
   core.preload('moon.png');
 
   core.onload = function() {
-    var bg = new Sprite(570, 570);
+    let bg = new Sprite(570, 570);
     bg.image = core.assets['moon.png'];
     core.rootScene.addChild(bg);
 
     // ラベル1　初期設定
-    var label1 = new Label("大学図書館員のための<br>Webアプリ開発本");
+    let label1 = new Label("大学図書館員のための<br>Webアプリ開発本");
     label1.color = '#0000FF';
     label1.font =  "italic bold'HG正楷書体-PRO'";
     label1.tl.scaleTo(2,3,1);
@@ -24,7 +24,7 @@ window.onload = function() {
     core.rootScene.addChild(label1);
 
     // ラベル2　初期設定
-    var label2 = new Label("前田朗著");
+    let label2 = new Label("前田朗著");
     label2.color = 'green';
     label2.font =  "30px 'HG正楷書体-PRO'";
     label2.x = 500;
@@ -32,13 +32,13 @@ window.onload = function() {
     core.rootScene.addChild(label2);
 
     // ラベル3　初期設定
-    var label3 = new Label("刊行予定とかなし！");
+    let label3 = new Label("刊行予定とかなし！");
     label3.color = 'red';
     label3.font =  "35px 'HG正楷書体-PRO'";
     label3.x = 100;
     label3.y = 500;
 
-    var count = 0;
+    let count = 0;
     this.addEventListener('enterframe', function() {
       // label1 弧を描く移動
       if (count < 320) {
